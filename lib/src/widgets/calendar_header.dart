f2312e63351b5136c6ff25665c97d4c3484c47df
XIATONG <Lawliet10478@gmail.com>
2023-11-03 11:15:15 +0700

    title header Xiatong

--- lib/src/widgets/calendar_header.dart
+++ lib/src/widgets/calendar_header.dart
@@ -62,7 +62,7 @@ class CalendarHeader extends StatelessWidget {
                   onTap: onHeaderTap,
                   onLongPress: onHeaderLongPress,
                   child: Text(
-                    text,
+                   "Xiatong",// text,
                     style: headerStyle.titleTextStyle,
                     textAlign: headerStyle.titleCentered
                         ? TextAlign.center
